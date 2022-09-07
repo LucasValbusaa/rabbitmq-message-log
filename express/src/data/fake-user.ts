059567b9efdd5bada9b9a1ec8ef9fb465c3a8ad3
lucas_valbusa <lucasvalbusagit@gmail.com>
2022-09-07 15:38:50 -0300

    feat: change fake user array

--- express/src/data/fake-user.ts
+++
@@ -1,4 +0,0 @@
-export const fakeUser = {
-  name: "lucas",
-  password: "3cd8d7e47d1d18136ec39cc08d1fbcf792a8cef7",
-};
---
+++ express/src/data/fake-users.ts
@@ -0,0 +1,14 @@
+export const fakeUsers = [
+  {
+    name: "lucas",
+    password: "123",
+  },
+  {
+    name: "pedro",
+    password: "123",
+  },
+  {
+    name: "valbusa",
+    password: "123",
+  },
+];

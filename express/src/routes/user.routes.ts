5fee1da86102ba86ef076a658df61b94b043559c
lucas_valbusa <lucasvalbusagit@gmail.com>
2022-09-07 15:39:20 -0300

    feat: create router login and data

--- express/src/routes/user.routes.ts
+++ express/src/routes/user.routes.ts
@@ -1,20 +1,28 @@
-import { fakeUser } from "@/data/fake-user";
+import { fakeUsers } from "@/data/fake-users";
 import { compareKeys } from "@/validators/keys-validation";
 import env from "@/configs/envs";
 import { Router } from "express";
 import { sign } from "jsonwebtoken";
+import logs from "@/middlewares/logs";
+import authenticate from "@/middlewares/authenticate";
 
 export default (router: Router): void => {
-  router.post("/users/authenticate", (req, res) => {
-    const isValidKeys = compareKeys(fakeUser, req.body);
-    if (!isValidKeys) return res.status(400).json({ error: "Missing params" });
-
-    if (fakeUser.password !== req.body.password) {
-      return res.status(403).json({ error: "Unauthorized" });
+  router.post("/login", async (req, res) => {
+    for (const user of fakeUsers) {
+      const isValidKeys = compareKeys(user, req.body);
+      if (!isValidKeys)
+        return res.status(400).json({ error: "Missing params" });
     }
 
-    const token = sign(fakeUser, env.token_screet);
+    const found = fakeUsers.find((user) => user.password === req.body.password);
+    if (!found) return res.status(404).json({ error: "User not found" });
+
+    const token = sign({ username: req.body.name }, env.token_screet);
 
     return res.json({ token });
+  });
+
+  router.get("/data", authenticate, logs, (req, res) => {
+    return res.json({ data: "any_data" });
   });
 };

78f2f8030f839e08083e998f5ca2fb771593cb3a
lucas_valbusa <lucasvalbusagit@gmail.com>
2022-09-07 15:41:04 -0300

    feat: create service and controller getLogs

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/rabbitmq/logs')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getLogs(): Promise<string> {
+    return await this.appService.getLogs();
   }
 }
--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { RabbitmqServer } from './providers/rabbitmq/rabbitmq-server';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getLogs(): Promise<any> {
+    const messages: Buffer[] = [];
+    const server = new RabbitmqServer('amqp://guest:guest@rabbitmq:5672');
+    await server.start();
+    await server.consume('nest', (message) => {
+      messages.push(message.content);
+    });
+
+    if (!messages.length) return { message: 'queue is empity' };
+
+    const formatedMessages = messages.map((m) => {
+      return JSON.parse(m.toString());
+    });
+
+    return { rabbitmqMessages: formatedMessages };
   }
 }
